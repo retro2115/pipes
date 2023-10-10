23a43650baa0ce98b10ebc070242f3763e016b95
Mirza Cerim <retro2115@outlook.com>
2023-10-10 23:39:39 +0200

    finishing pipes

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, targetUnits: string): unknown {
+	console.log(value)
+	if (!value) {
+		return ''
+	}
+    
+	switch(targetUnits) {
+		case 'km':
+			return value * 1.60934;
+		case 'm':
+			return value * 1.60394 * 1000;
+		case 'cm':
+			return	value * 1.60394 * 1000 * 1000;
+		default:
+			throw new Error('Target unit not supported')
+	} 
   }
 
 }
